51ec950c2e9c6d346e79dfeefe8090e4bc28c43c
Gary Willoughby <dev@kalekold.net>
2014-04-08 12:01:12 +0100

    Finished implementing the class and style enums.

--- source/tkd/element/elementclass.d
+++ source/tkd/element/elementclass.d
@@ -11,7 +11,24 @@ module tkd.element.elementclass;
  */
 enum ElementClass : string
 {
-	button = "TButton",
-	frame  = "TFrame",
-	entry  = "TEntry",
+	button      = "TButton",      /// Button widget class.
+	canvas      = "Canvas",       /// Canvas widget class.
+	checkButton = "TCheckbutton", /// CheckButton widget class.
+	comboBox    = "TCombobox",    /// ComboBox widget class.
+	entry       = "TEntry",       /// Entry widget class.
+	frame       = "TFrame",       /// Frame widget class.
+	label       = "TLabel",       /// Label widget class.
+	labelFrame  = "TLabelframe",  /// LabelFrame widget class.
+	menuButton  = "TMenubutton",  /// MenuButton widget class.
+	noteBook    = "TNotebook",    /// NoteBook widget class.
+	panedWindow = "TPanedwindow", /// PanedWindow widget class.
+	progressBar = "TProgressbar", /// ProgressBar widget class.
+	radioButton = "TRadiobutton", /// RadioButton widget class.
+	scale       = "TScale",       /// Scale widget class.
+	scrollBar   = "TScrollbar",   /// ScrollBar widget class.
+	separator   = "TSeparator",   /// Separator widget class.
+	sizeGrip    = "TSizegrip",    /// SizeGrip widget class.
+	spinBox     = "TSpinbox",     /// SpinBox widget class.
+	text        = "Text",         /// Text widget class.
+	treeView    = "Treeview",     /// Treeview widget class.
 }
--- source/tkd/widget/style.d
+++ source/tkd/widget/style.d
@@ -11,8 +11,26 @@ module tkd.widget.style;
  */
 enum Style : string
 {
-	button     = "TButton",
-	frame      = "TFrame",
-	entry      = "TEntry",
-	toolbutton = "Toolbutton",
+	button      = "TButton",      /// Button widget style.
+	canvas      = "Canvas",       /// Canvas widget style.
+	checkButton = "TCheckbutton", /// CheckButton widget style.
+	comboBox    = "TCombobox",    /// ComboBox widget style.
+	entry       = "TEntry",       /// Entry widget style.
+	frame       = "TFrame",       /// Frame widget style.
+	label       = "TLabel",       /// Label widget style.
+	labelFrame  = "TLabelframe",  /// LabelFrame widget style.
+	menuButton  = "TMenubutton",  /// MenuButton widget style.
+	noteBook    = "TNotebook",    /// NoteBook widget style.
+	panedWindow = "TPanedwindow", /// PanedWindow widget style.
+	progressBar = "TProgressbar", /// ProgressBar widget style.
+	radioButton = "TRadiobutton", /// RadioButton widget style.
+	scale       = "TScale",       /// Scale widget style.
+	scrollBar   = "TScrollbar",   /// ScrollBar widget style.
+	separator   = "TSeparator",   /// Separator widget style.
+	sizeGrip    = "TSizegrip",    /// SizeGrip widget style.
+	spinBox     = "TSpinbox",     /// SpinBox widget style.
+	text        = "Text",         /// Text widget style.
+	treeView    = "Treeview",     /// Treeview widget style.
+
+	toolbutton = "Toolbutton",    /// Toolbutton style. Useful for styling widget for use on a toolbar.
 }
