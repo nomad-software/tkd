ba7772253160a46f9a8ad0016c14883a6c7d93f8
Andrew Grim <39337035+AndrewGrim@users.noreply.github.com>
2019-04-28 11:43:50 +0100

    added size, bold, italic, underline options for setFont

--- source/tkd/widget/common/font.d
+++ source/tkd/widget/common/font.d
@@ -11,9 +11,9 @@ module tkd.widget.common.font;
  */
 mixin template Font()
 {  
-	public auto setFont(this T)(string font)
+	public auto setFont(this T)(string font, int size = 12, string bold = "", string italic = "", string underline = "")
 	{
-		this._tk.eval("%s configure -font %s", this.id, font);
+		this._tk.eval("%s configure -font {%s %s %s %s %s}", this.id, font, size, bold, italic, underline);
 
 		return cast(T) this;
 	}
